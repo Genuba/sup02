a2f1b3c15b6e9a71f3c8c0bee2f9d702af94e83b
genuba <daalpiva@gmail.com>
2020-09-05 15:40:41 -0500

    with love

--- 02_web/01_client/src/app/components/admin-gestor/dto/idtoReceiver.ts
+++
@@ -1,8 +0,0 @@
-export interface IDtoReceiver {
-    rec_rec: number;   
-    rec_name: string;
-    rec_address: string;
-    rec_email: string;
-    rec_desc: string;
-    rec_telf: string;
- } 
--- 02_web/01_client/src/app/components/admin-gestor/dto/idtoType.ts
+++
@@ -1,7 +0,0 @@
-export interface IDtoType {
-    type_type: number;   
-    type_class: string;
-    type_name: string;
-    type_code: string;
-    type_rute: string;
- } 
